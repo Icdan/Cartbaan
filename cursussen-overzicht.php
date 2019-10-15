98dc6e25f610dd250ed8b5a4c11da48e75df8236
Icdan <li215@live.nl>
2019-10-15 13:38:06 +0200

    Course overview and allows for deletion of courses

--- cursussen-overzicht.php
+++ cursussen-overzicht.php
@@ -1,1 +1,61 @@
 <?php
+//Start the session to work with PHP sessions
+session_start();
+//Connect to database
+include "db/db_connection.php";
+//If user isn't logged in they'll be redirected to the log-in page.
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS files -->
+    <?php
+    include "includes/header.php";
+    ?>
+    <title>Cursussen overzicht</title>
+</head>
+<body>
+<?php
+include "includes/navbar.php";
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table>
+                <tr>
+                    <th>Omschrijving</th>
+                    <th>Wijzigen</th>
+                </tr>
+                <?php
+                //Select and show all courses
+                $getCarts = mysqli_query($conn, "SELECT * FROM `cursus`");
+                //If we're able to get any courses, check how many there are and loop through the results, creating a table row with data for each of them
+                //Also adds a button to delete a course
+                if ($getCarts) {
+                    $amountOfCarts = mysqli_num_rows($getCarts);
+                    for ($count = 1; $count <= $amountOfCarts; $count++) {
+                        $row = mysqli_fetch_assoc($getCarts);
+                        echo "<tr><td>" . $row['omschrijving'] ."</td><td>" . "<form method='post'><input type='submit' name='verwijder' value ='Verwijder'><input type='hidden' name='idcursus' value='" . $row['idcursus'] . "'></form></td></tr>";
+                    }
+                }
+                //If something is selected to be deleted, it will look at the id of the item being deleted and force a refresh of the page in order to make sure the course overview is updated
+                if (isset($_POST['verwijder'])) {
+                    $id = $_POST['idcursus'];
+                    mysqli_query($conn, "DELETE FROM cursus WHERE `idcursus` = '$id'");
+                    header("Location: cursussen-overzicht.php");
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
+<!-- Javascript files -->
+<?php
+include "includes/footer.php";
+?>
+</body>
+</html>
